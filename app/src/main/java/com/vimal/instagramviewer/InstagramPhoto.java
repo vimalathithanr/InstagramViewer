c8fea9ecf96583a54c6e4add5ffda831d27a963a
sankarbala123 <vimalathithanit@gmail.com>
2016-02-02 23:12:54 -0800

    Inital Commit

---
+++ app/src/main/java/com/vimal/instagramviewer/InstagramPhoto.java
@@ -0,0 +1,15 @@
+package com.vimal.instagramviewer;
+
+/**
+ * Created by FearMeNot on 2/2/2016.
+ */
+public class InstagramPhoto {
+    public String username;
+    public String caption;
+    public String imageUrl;
+    public int imageHeight;
+    public int likesCount;
+
+
+
+}
---
+++ app/src/main/java/com/vimal/instagramviewer/PhotosActivity.java
@@ -0,0 +1,118 @@
+package com.vimal.instagramviewer;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
+
+public class PhotosActivity extends AppCompatActivity {
+
+    public static final String CLIENT_ID = "e05c462ebd86446ea48a5af73769b602";
+    private ArrayList<InstagramPhoto> photos;
+    private InstagramPhotosAdapter aPhotos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photos);
+        photos = new ArrayList<>();
+        aPhotos = new InstagramPhotosAdapter(this, photos);
+        ListView lvPhotos = (ListView) findViewById(R.id.lvPhotos);
+        lvPhotos.setAdapter(aPhotos);
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        fetchPopularPhotos();
+
+    }
+
+    public void fetchPopularPhotos() {
+
+        String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(url, new JsonHttpResponseHandler() {
+
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                //super.onSuccess(statusCode, headers, response);
+                Log.i("DEBUG", response.toString());
+
+                JSONArray photosJSON = null;
+
+                try {
+                    photosJSON = response.getJSONArray("data");
+
+                    for (int i = 0; i < photosJSON.length(); i++) {
+                        JSONObject photoJSON = photosJSON.getJSONObject(i);
+                        InstagramPhoto photo = new InstagramPhoto();
+                        photo.username = photoJSON.getJSONObject("user").getString("username");
+                        photo.caption = photoJSON.getJSONObject("caption").getString("text");
+                        photo.imageUrl = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
+                        photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
+                        photo.likesCount = photoJSON.getJSONObject("likes").getInt("count");
+                        photos.add(photo);
+                    }
+
+                } catch (JSONException e) {
+
+                }
+
+                aPhotos.notifyDataSetChanged();
+            }
+
+//            @Override
+//            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
+//                //super.onFailure(statusCode, headers, responseString, throwable);
+//            }
+
+
+            @Override
+            public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
+                //super.onFailure(statusCode, headers, throwable, errorResponse);
+            }
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_photos, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
