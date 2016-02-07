e031c7c9c6d7c32fd62cfaa470a7d2c9949d5da8
sankarbala123 <vimalathithanit@gmail.com>
2016-02-07 09:54:56 -0800

    Fixed swipe refresh layout bug

--- app/src/main/java/com/vimal/instagramviewer/PhotosActivity.java
+++ app/src/main/java/com/vimal/instagramviewer/PhotosActivity.java
@@ -26,12 +26,15 @@ public class PhotosActivity extends AppCompatActivity {
     private ArrayList<InstagramPhoto> photos;
     private InstagramPhotosAdapter aPhotos;
     private SwipeRefreshLayout swipeContainer;
+    private String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
+    AsyncHttpClient client = new AsyncHttpClient();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_photos);
+
         photos = new ArrayList<>();
         aPhotos = new InstagramPhotosAdapter(this, photos);
 
@@ -41,12 +44,11 @@ public class PhotosActivity extends AppCompatActivity {
         swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
             @Override
             public void onRefresh() {
-
-                fetchTimelineAsync(0);
+                fetchPopularPhotos();
                 swipeContainer.setRefreshing(false);
-
             }
         });
+
         // Configure the refreshing colors
         swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
                 android.R.color.holo_green_light,
@@ -61,67 +63,16 @@ public class PhotosActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         fetchPopularPhotos();
-        swipeContainer.setRefreshing(false);
-
-
     }
-
-    public void fetchTimelineAsync(int page) {
-        // Send the network request to fetch the updated data
-        // `client` here is an instance of Android Async HTTP
-
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
-        AsyncHttpClient client = new AsyncHttpClient();
-
-        client.get(url, new JsonHttpResponseHandler() {
-            public void onSuccess(JSONObject json) {
-                JSONArray photosJSON = null;
-
-                try {
-                    photosJSON = json.getJSONArray("data");
-
-                    for (int i = 0; i < photosJSON.length(); i++) {
-                        JSONObject photoJSON = photosJSON.getJSONObject(i);
-                        InstagramPhoto photo = new InstagramPhoto();
-                        photo.username = photoJSON.getJSONObject("user").getString("username");
-                        photo.caption = photoJSON.getJSONObject("caption").getString("text");
-                        photo.timeCreated = photoJSON.getJSONObject("caption").getLong("created_time");
-                        photo.imageUrl = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getString("url");
-                        photo.imageHeight = photoJSON.getJSONObject("images").getJSONObject("standard_resolution").getInt("height");
-                        photo.likesCount = photoJSON.getJSONObject("likes").getInt("count");
-                        photo.profileUrl = photoJSON.getJSONObject("user").getString("profile_picture");
-                        photos.add(photo);
-                    }
-
-                } catch (JSONException e) {
-
-                }
-
-                swipeContainer.setRefreshing(false);
-            }
-
-            public void onFailure(Throwable e) {
-                Log.d("DEBUG", "Fetch timeline error: " + e.toString());
-            }
-        });
-    }
-
 
     public void fetchPopularPhotos() {
 
-        String url = "https://api.instagram.com/v1/media/popular?client_id=" + CLIENT_ID;
-
-        AsyncHttpClient client = new AsyncHttpClient();
 
         client.get(url, new JsonHttpResponseHandler() {
 
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
-                //super.onSuccess(statusCode, headers, response);
-                Log.i("DEBUG", response.toString());
+                aPhotos.clear();
 
                 JSONArray photosJSON = null;
 
@@ -142,12 +93,11 @@ public class PhotosActivity extends AppCompatActivity {
                         JSONArray jsonComments = photoJSON.getJSONObject("comments").getJSONArray("data");
                         photo.comment = new String[jsonComments.length()];
 
-                        for(int j=photo.comment.length-1;j>=0;j--) {
+                        for (int j = photo.comment.length - 1; j >= 0; j--) {
                             JSONObject jsonData = jsonComments.getJSONObject(j);
                             photo.comment[j] = jsonData.getJSONObject("from").getString("username") + " : " + jsonData.getString("text");
                         }
 
-                        //Log.i("DebugJSON", photoJSON.getJSONObject("comments").getJSONArray("data").get(0).toString());
                         photos.add(photo);
                     }
 
@@ -158,12 +108,6 @@ public class PhotosActivity extends AppCompatActivity {
                 aPhotos.notifyDataSetChanged();
             }
 
-//            @Override
-//            public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
-//                //super.onFailure(statusCode, headers, responseString, throwable);
-//            }
-
-
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 //super.onFailure(statusCode, headers, throwable, errorResponse);
@@ -173,19 +117,14 @@ public class PhotosActivity extends AppCompatActivity {
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_photos, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
